73c89202b36caded864e617b295081b3ea24334b
Yanis Berger <120224002+yanisberger@users.noreply.github.com>
2023-12-11 13:39:38 +0100

    delay bueno

--- PrototypeProject/deepfaceTest.py
+++ PrototypeProject/deepfaceTest.py
@@ -1,35 +1,31 @@
 from deepface import DeepFace
 import cv2
 import time
+import threading
 
 highestScore = 0.01
 bestMeme = 0
 
 def takePic(MemePosition, cap, MemeDictionary):
 
-    global highestScore
-    global bestMeme
+    def delayed_capture():
+        global highestScore
+        global bestMeme
+        time.sleep(2)  # 2 seconds delay
+        # Write the rest of the capture logic here
+        while not cap.isOpened():
+            pass
+        ret, frame = cap.read()
+        if ret:
+            cv2.imwrite(f"WebCamShots/{MemePosition}.jpg", frame)
 
-    # Verzögerung von 2 sekunden bis Bild aufgenommen wird ACHTUNG BLOCKT ALLES? NOCH KEINE GUTE ASYNCHRONE LÖSUNG GEFUNDEN
-    #time.sleep(2)
-    # Warte auf die Initialisierung der Kamera
-    while not cap.isOpened():
-        pass
-    # Lese ein Bild von der Webcam
-    ret, frame = cap.read()
-    # Überprüfe, ob das Lesen erfolgreich war
-    if ret:
-        # Speichere das Bild
-        cv2.imwrite(f"WebCamShots/{MemePosition}.jpg", frame)
+        analyzRes = DeepFace.analyze(f"WebCamShots/{MemePosition}.jpg", actions=['emotion'])
+        happyPercent = analyzRes[0]['emotion']['happy']
+        MemeDictionary[MemePosition] = happyPercent
 
-    # Analyziere Bild
-    analyzRes = DeepFace.analyze(f"WebCamShots/{MemePosition}.jpg",
-                            actions=['emotion'])
+        if happyPercent > highestScore:
+            highestScore = happyPercent
+            bestMeme = MemePosition
 
-    # save absolute values of a certain emotion, why 0 idk
-    happyPercent = (analyzRes[0]['emotion']['happy'])
-    MemeDictionary[MemePosition] = happyPercent
-
-    if happyPercent > highestScore:
-        highestScore = happyPercent
-        bestMeme = MemePosition
+    # Start a thread to capture the image after a delay
+    threading.Thread(target=delayed_capture).start()
